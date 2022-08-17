aff374451b2de2d6f47a75dd6decbd7eb892bde6
bienao666 <1319174340@qq.com>
2022-08-17 18:49:31 +0800

    初始化

--- src/main/java/com/bienao/robot/RobotApplication.java
+++ src/main/java/com/bienao/robot/RobotApplication.java
@@ -1,9 +1,13 @@
 package com.bienao.robot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan("com.bienao.robot.mapper")
+@EnableScheduling //开启定时任务
 public class RobotApplication {
 
     public static void main(String[] args) {
