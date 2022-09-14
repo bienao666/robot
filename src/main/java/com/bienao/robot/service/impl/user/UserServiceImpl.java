e1c5fa55b791e99206826e8a9dadc211a7c13c8e
bienao666 <1319174340@qq.com>
2022-09-14 23:41:41 +0800

    登陆

--- src/main/java/com/bienao/robot/service/impl/user/UserServiceImpl.java
+++ src/main/java/com/bienao/robot/service/impl/user/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.bienao.robot.service.impl.user;
 import com.bienao.robot.entity.SystemParam;
 import com.bienao.robot.entity.User;
 import com.bienao.robot.enums.ErrorCodeConstant;
+import com.bienao.robot.mapper.SystemParamMapper;
 import com.bienao.robot.result.Result;
 import com.bienao.robot.service.user.UserService;
 import com.bienao.robot.utils.systemParam.SystemParamUtil;
@@ -18,12 +19,15 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private SystemParamUtil systemParamUtil;
 
+    @Autowired
+    private SystemParamMapper systemParamMapper;
+
     @Override
     public User getUser(String username, String password){
-        List<SystemParam> systemParams = systemParamUtil.querySystemParams("USERNAME");
-        String userName = systemParams.get(0).getValue();
-        systemParams = systemParamUtil.querySystemParams("PASSWORD");
-        String passWord = systemParams.get(0).getValue();
+        SystemParam systemParams = systemParamMapper.querySystem("USERNAME");
+        String userName = systemParams.getValue();
+        systemParams = systemParamMapper.querySystem("PASSWORD");
+        String passWord = systemParams.getValue();
 
         if (userName.equals(username) && passWord.equals(password)){
             User user=new User();
@@ -38,10 +42,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String username){
-        List<SystemParam> systemParams = systemParamUtil.querySystemParams("USERNAME");
-        String userName = systemParams.get(0).getValue();
-        systemParams = systemParamUtil.querySystemParams("PASSWORD");
-        String passWord = systemParams.get(0).getValue();
+        SystemParam systemParams = systemParamMapper.querySystem("USERNAME");
+        String userName = systemParams.getValue();
+        systemParams = systemParamMapper.querySystem("PASSWORD");
+        String passWord = systemParams.getValue();
         if (userName.equals(username)){
             User user=new User();
             user.setUserName(userName);
@@ -55,10 +59,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Result check() {
-        List<SystemParam> systemParams = systemParamUtil.querySystemParams("USERNAME");
-        String userName = systemParams.get(0).getValue();
-        systemParams = systemParamUtil.querySystemParams("PASSWORD");
-        String passWord = systemParams.get(0).getValue();
+        SystemParam systemParams = systemParamMapper.querySystem("USERNAME");
+        String userName = systemParams.getValue();
+        systemParams = systemParamMapper.querySystem("PASSWORD");
+        String passWord = systemParams.getValue();
         if (StringUtils.isEmpty(userName) && StringUtils.isEmpty(passWord)){
             //未注册过
             return Result.error(ErrorCodeConstant.NO_REGISTER_ERROR,"账号尚未注册");
@@ -69,13 +73,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Result register(String username, String password) {
-        List<SystemParam> systemParams = systemParamUtil.querySystemParams("USERNAME");
-        String userName = systemParams.get(0).getValue();
-        systemParams = systemParamUtil.querySystemParams("PASSWORD");
-        String passWord = systemParams.get(0).getValue();
+        SystemParam systemParams = systemParamMapper.querySystem("USERNAME");
+        String userName = systemParams.getValue();
+        systemParams = systemParamMapper.querySystem("PASSWORD");
+        String passWord = systemParams.getValue();
         if (StringUtils.isEmpty(userName) && StringUtils.isEmpty(passWord)){
-            systemParamUtil.updateSystemParam("USERNAME",null,userName);
-            systemParamUtil.updateSystemParam("PASSWORD",null,passWord);
+            systemParamUtil.updateSystemParam("USERNAME",null,username);
+            systemParamUtil.updateSystemParam("PASSWORD",null,password);
             return Result.success();
         }else {
             return Result.error(ErrorCodeConstant.REGISTER_ERROR,"已注册过，注册失败");
