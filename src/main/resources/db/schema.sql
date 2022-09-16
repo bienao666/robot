9a4e468075f9adfb95883125195153f38866e286
dwtian <dwtian@sedwt.com.cn>
2022-09-16 17:37:49 +0800

    助力池代码迁移

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -7,8 +7,8 @@ create table if not exists systemParam (
                                            codeName varchar,
                                            value varchar,
                                            isShow INTEGER DEFAULT 1,
-                                           created_time datetime DEFAULT CURRENT_TIMESTAMP,
-                                           updated_time datetime DEFAULT CURRENT_TIMESTAMP
+                                           created_time datetime DEFAULT (datetime('now', 'localtime')),
+                                           updated_time datetime DEFAULT (datetime('now', 'localtime'))
 );
 create table if not exists user (
                                            id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,16 +17,16 @@ create table if not exists user (
                                            city varchar,
                                            ip varchar,
                                            functionType INTEGER,
-                                           created_time datetime DEFAULT CURRENT_TIMESTAMP,
-                                           updated_time datetime DEFAULT CURRENT_TIMESTAMP
+                                           created_time datetime DEFAULT (datetime('now', 'localtime')),
+                                           updated_time datetime DEFAULT (datetime('now', 'localtime'))
 );
 create table if not exists `group` (
                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                                        groupid varchar,
                                        groupName varchar,
                                        functionType INTEGER,
-                                       created_time datetime DEFAULT CURRENT_TIMESTAMP,
-                                       updated_time datetime DEFAULT CURRENT_TIMESTAMP
+                                       created_time datetime DEFAULT (datetime('now', 'localtime')),
+                                       updated_time datetime DEFAULT (datetime('now', 'localtime'))
 );
 create table if not exists `ql` (
                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -35,31 +35,81 @@ create table if not exists `ql` (
                                        clientSecret INTEGER,
                                        head varchar,
                                        remark varchar,
-                                       created_time datetime DEFAULT CURRENT_TIMESTAMP,
-                                       updated_time datetime DEFAULT CURRENT_TIMESTAMP
+                                       created_time datetime DEFAULT (datetime('now', 'localtime')),
+                                       updated_time datetime DEFAULT (datetime('now', 'localtime'))
 );
 create table if not exists `wire` (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     activity_name varchar,
                                     script varchar UNIQUE,
                                     status varchar DEFAULT '未执行',
-                                    created_time datetime DEFAULT CURRENT_TIMESTAMP,
-                                    updated_time datetime DEFAULT CURRENT_TIMESTAMP
+                                    created_time datetime DEFAULT (datetime('now', 'localtime')),
+                                    updated_time datetime DEFAULT (datetime('now', 'localtime'))
 );
 create table if not exists `wireKey` (
                                       id INTEGER PRIMARY KEY AUTOINCREMENT,
                                       wireId INTEGER,
                                       key varchar,
-                                      created_time datetime DEFAULT CURRENT_TIMESTAMP,
-                                      updated_time datetime DEFAULT CURRENT_TIMESTAMP
+                                      created_time datetime DEFAULT (datetime('now', 'localtime')),
+                                      updated_time datetime DEFAULT (datetime('now', 'localtime'))
 );
 create table if not exists `wirelist` (
                                          id INTEGER PRIMARY KEY AUTOINCREMENT,
                                          script varchar,
                                          content varchar UNIQUE,
                                          result varchar,
-                                         created_time datetime DEFAULT CURRENT_TIMESTAMP,
-                                         updated_time datetime DEFAULT CURRENT_TIMESTAMP
+                                         created_time datetime DEFAULT (datetime('now', 'localtime')),
+                                         updated_time datetime DEFAULT (datetime('now', 'localtime'))
 );
 alter table `ql` add `tokenType` varchar after clientSecret;
 alter table `ql` add `token` varchar after tokenType;
+create table if not exists `jdck` (
+                                      `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                                      `ck` varchar,
+                                      `pt_pin` varchar,
+                                      `expiry` INTEGER DEFAULT '-1',
+                                      `remark` varchar,
+                                      `status` INTEGER DEFAULT '0',
+                                      `level` INTEGER DEFAULT '2',
+                                      `jd` INTEGER DEFAULT '0',
+                                      `created_time` datetime DEFAULT (datetime('now', 'localtime')),
+                                      `updated_time` datetime DEFAULT (datetime('now', 'localtime'))
+);
+create table if not exists `jdFruit` (
+                           `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                           `help_code` varchar,
+                           `ckid` INTEGER,
+                           `is_fruit_hei` INTEGER DEFAULT '0',
+                           `help_status` INTEGER DEFAULT '0',
+                           `to_help_status` INTEGER DEFAULT '1',
+                           `help_lottery_status` INTEGER DEFAULT '0',
+                           `to_help_lottery_status` INTEGER DEFAULT '1',
+                           `created_time` datetime DEFAULT (datetime('now', 'localtime')),
+                           `updated_time` datetime DEFAULT (datetime('now', 'localtime'))
+);
+create table if not exists `jdjd` (
+                                      `date` timestamp PRIMARY KEY,
+                                      `jdCount` INTEGER DEFAULT '0',
+                                      `created_time` datetime DEFAULT (datetime('now', 'localtime')),
+                                      `updated_time` datetime DEFAULT (datetime('now', 'localtime'))
+);
+create table if not exists `jdPet` (
+                         `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                         `help_code` varchar,
+                         `ckid` INTEGER,
+                         `is_pet_hei` INTEGER DEFAULT '0',
+                         `help_status` INTEGER DEFAULT '0',
+                         `to_help_status` INTEGER DEFAULT '1',
+                         `created_time` datetime DEFAULT (datetime('now', 'localtime')),
+                         `updated_time` datetime DEFAULT (datetime('now', 'localtime'))
+);
+create table if not exists `jdPlant` (
+                           `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                           `help_code` varchar,
+                           `ckid` INTEGER,
+                           `is_Plant_hei` INTEGER DEFAULT '0',
+                           `help_status` INTEGER DEFAULT '0',
+                           `to_help_status` INTEGER DEFAULT '1',
+                           `created_time` datetime DEFAULT (datetime('now', 'localtime')),
+                           `updated_time` datetime DEFAULT (datetime('now', 'localtime'))
+);
