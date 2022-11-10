f6ca5f92e6aafba508f55a97a3debde801afbbba
dwtian <dwtian@sedwt.com.cn>
2022-11-10 16:41:40 +0800

    测试

--- src/main/java/com/bienao/robot/controller/jingdong/CkController.java
+++ src/main/java/com/bienao/robot/controller/jingdong/CkController.java
@@ -1,11 +1,13 @@
 package com.bienao.robot.controller.jingdong;
 
+import cn.hutool.cache.Cache;
 import com.alibaba.fastjson.JSONObject;
 import com.bienao.robot.annotation.LoginToken;
 import com.bienao.robot.annotation.PassToken;
 import com.bienao.robot.entity.Result;
 import com.bienao.robot.entity.jingdong.JdCkEntity;
 import com.bienao.robot.enums.ErrorCodeConstant;
+import com.bienao.robot.redis.Redis;
 import com.bienao.robot.service.jingdong.CkService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
