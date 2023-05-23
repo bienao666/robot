c1c7f3d1562c039512276a158462d45f33b997f2
bienao666 <1319174340@qq.com>
2023-05-23 23:31:19 +0800

    筛选激活

--- src/test/java/com/bienao/robot/RobotApplicationTests.java
+++ src/test/java/com/bienao/robot/RobotApplicationTests.java
@@ -3,7 +3,8 @@ package com.bienao.robot;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.net.URLEncoder;
 import cn.hutool.http.HttpRequest;
-import com.alibaba.fastjson.JSONObject;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.XML;
 import com.bienao.robot.entity.QlEnv;
 import com.bienao.robot.service.ql.QlService;
 import com.bienao.robot.service.weixin.WxService;
@@ -17,6 +18,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.nio.charset.Charset;
 import java.util.HashSet;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootTest
 class RobotApplicationTests {
@@ -70,6 +73,47 @@ class RobotApplicationTests {
             qlUtil.addEnvs(url2, tokenType2, token2, env.getName(),env.getValue(),env.getRemarks());
         }
     }*/
+    public static void main(String[] args) {
+        String host = "http://jetbrains-lic.novx.org";
+        try {
+            /*Pattern pattern = Pattern.compile("http[s]?://");
+            Matcher matcher = pattern.matcher(host);
+            String protocol = "";
+            if (matcher.find()) {
+                protocol = matcher.group();
+            }
+            String hostWithoutProtocol = host.replace(protocol, "");*/
+
+            String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0";
+            String aliveUrl = "/rpc/ping.action?salt=1669702326321";
+            String activeUrl = "/rpc/obtainTicket.action?" +
+                    "machineId=cc696c3f-d41e-45b6-9d12-59e6d2250171&" +
+                    "productCode=49c202d4-ac56-452b-bb84-735056242fb3&" +
+                    "salt=1669702326321&" +
+                    "userName=FuckYou&" +
+                    "hostName=DESKTOP-FuckYou";
+
+            String res = HttpRequest.get(host + aliveUrl)
+                    .header("user-agent", userAgent)
+                    .timeout(5000)
+                    .execute().body();
+            JSONObject jsonObject = XML.toJSONObject(res).getJSONObject("PingResponse");
+            if (jsonObject.getStr("responseCode").equals("OK")){
+                res = HttpRequest.get(host + activeUrl)
+                        .header("user-agent", userAgent)
+                        .timeout(5000)
+                        .execute().body();
+                jsonObject = XML.toJSONObject(res).getJSONObject("ObtainTicketResponse");
+                if (jsonObject.getStr("responseCode").equals("OK")){
+                    System.out.println(jsonObject.getStr("ticketProperties"));
+                }else {
+                    System.out.println(jsonObject.getStr("message"));
+                }
+            }
+        } catch (Exception error) {
+            System.out.printf("[x]%s --- %s%n", host, error.getMessage());
+        }
+    }
 
 
 }
